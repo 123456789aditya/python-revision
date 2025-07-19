955b613fca6f0f9cc8ab50982df3154747e70961
aditya pathak <pathakaditya881@gmail.com>
2025-07-19 23:19:00 +0530

    added

---
+++ python/index.py
@@ -0,0 +1,34 @@
+my_list=[2,1,3,4,"aditya","neeraj","kumar"]
+# print(my_list)
+# print(max(my_list))
+# print(min(my_list))
+# print(sum(my_list))
+# print(id(my_list))
+
+# my_list.append("pathak")
+# print(my_list)
+
+# my_list.sort()
+# print(my_list)
+
+# my_list.extend("kumar")
+# print(my_list)
+
+# print(my_list)
+# my_list.remove("kumar")
+# print(my_list)
+
+# my_list.clear()
+# print(my_list)
+
+# my_list.pop()
+# print(my_list)
+# my_list.insert(1,87)
+# print(my_list)
+
+# my_list.copy(l1)
+
+d1={"name":"aditya","age":21,"quali":"btech"}
+d2={"city":"bhopal","college":"LNCT"}
+d1.update(d2)
+print(d1)
---
+++ python/tempCodeRunnerFile.py
@@ -0,0 +1,15 @@
+my_list=[2,1,3,4,"aditya","neeraj","kumar"]
+# print(my_list)
+# print(max(my_list))
+# print(min(my_list))
+# print(sum(my_list))
+# print(id(my_list))
+
+# my_list.append("pathak")
+# print(my_list)
+
+# my_list.sort()
+# print(my_list)
+
+# my_list.extend("kumar")
+# print(my_list)
