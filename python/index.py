1291da3de5f85b0f7a1f6db8e6a0a945eaec1aa8
aditya pathak <pathakaditya881@gmail.com>
2025-07-20 13:39:57 +0530

    added

--- python/index.py
+++ python/index.py
@@ -28,7 +28,100 @@ my_list=[2,1,3,4,"aditya","neeraj","kumar"]
 
 # my_list.copy(l1)
 
-d1={"name":"aditya","age":21,"quali":"btech"}
-d2={"city":"bhopal","college":"LNCT"}
-d1.update(d2)
-print(d1)
+# x=eval(input("enter first number"))
+# y=eval(input("enter second number"))
+# if x>y:
+#     smaller=y
+# else:
+#     smaller=x
+# for i in range(2,smaller+1):
+#     if((x%i==0) and (y%i==0)):
+#         hcf=i
+# print("hcf will be:",hcf)       
+
+# x=int(input("enter the range:"))
+# sum=0
+# for i in range(0,x+1):
+#     sum=sum+i
+# print(sum)    
+
+
+# def display(*n):
+#     sum=0
+#     for i in n:
+#         sum=sum+i
+#     return sum
+
+# print(display(1,2,3,4,5,6,7))
+
+# def details(**n):
+#     for k,v in n.items():
+#         print(f"my{k} is {v}")
+        
+# details(name="aditya",city="bhopal")
+
+# def details(**n):
+#     print(n)
+    
+# details(name="neeraj",city="Bhopal")
+
+# x=int(input("enter any number"))
+# sum=0
+# order=len(str(x))
+# copy_x=x
+# while(x>0):
+#     last_digit=x%10
+#     sum=sum+last_digit**order
+#     x=x//10
+# if(sum==copy_x):
+#     print("armstrong number:",sum)
+# else:
+#     print("not armstrong:",sum)
+    
+# x=int(input("enter any number"))
+# numb=x
+# sum=0
+# while(x>0):
+#     digit=x%10
+#     sum=sum*10+digit
+#     x=x//10
+# if(sum==numb):
+#     print("palindrome number")
+# else:
+#     print("not palindrome")
+
+# print("finding factorial")
+# x=int(input("enter any number for factorial"))
+# sum=1
+# while(x>0):
+#     sum=sum*x
+#     x=x-1
+# print("factorial is:",sum)
+
+# x=int(input("enter any number for factorial"))
+# num=1
+# sum=1
+# for i in range(x,2):
+ 
+#     sum=i*i-1
+# print(sum)
+
+# n=int(input("enter the number of colums"))
+# for i in range(n):
+#     for j in range(i+1):
+#         print("*",end="")
+#     print()
+        
+# n=int(input("enter column range"))
+# for i in range(n):
+#     for j in range(n-i):
+#         print("*",end="")
+#     print()
+    
+    
+
+    
+
+    
+        
+        
