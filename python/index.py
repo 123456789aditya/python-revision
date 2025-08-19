54f8db043b99409d3e3f4aaa6ab2cb7bf42235d7
aditya pathak <pathakaditya881@gmail.com>
2025-08-19 23:55:58 +0530

    added

--- python/index.py
+++ python/index.py
@@ -283,4 +283,190 @@ d1={"name":"aditya","surname":"pathak","rollno":"03"}
 #     print("str count is",string_count)
     
 # calculate("aditya234")
+     
+# n=int(input("enter a number:"))
+# temp=n
+# reverse=0
+# while(n>0):
+#     digit=n%10
+#     reverse=reverse*10+digit
+#     n=n//10
+# if(temp==reverse):
+#     print(f'{reverse} is a palindrome number')
+# else:
+#     print(f"{reverse} is not a palindrome number")
+
+# str1=input("enter first number")
+# str2=input("enter second number")
+# if (sorted(str1)==sorted(str2)):
+#     print("anagram")
+# else:
+#     print("not anagram")       
+
+
+
+# a=input("enter any word")
+# vowel=0
+# consonant=0
+# for i in range(0,len(a)):
+#     if (a[i]!=""):
+#         if (a[i]=='a'or a[i]=='e',a[i]=='i',a[i]=='o',a[i]=='u'):
+#             vowel=vowel+1
+#         else:
+#             consonant=consonant+1
+# print("vowels are",vowel)
+# print("conso are:",consonant)
+    
+#------------------------Advance python
+
+# def cube(x):
+#     return x*x*x
+
+# l=[1,2,3,4,5,6,7,8,9]
+
+# new=map(cube,l)
+# print(tuple((new)))
+
+# def square(x):
+#     return x**2
+
+# n=eval(input("enter any list"))
+# res=map(square,n)
+# print(list(res))
+
+# def display(x):
+#     if x%2==0:
+#         return x+1
+#     else:
+#         return x+2
+    
+# n=eval(input("enter any list"))
+# print(list(map(display,n)))
+
+# def display(x,y,z):
+#     return x+y+z
+
+# l1=[2,4,6]
+# l2=[1,2,3]
+# l3=[4,7,2,8,6,9,10]
+# res=map(display,l1,l2,l3)
+# print(list(res))
+
+# def new(x):
+#     return x>4
+
+# l1=[1,2,3,4,5,6,7,8,9,10]
+# res=filter(new,l1)
+# print(list(res))
+
+# from functools import reduce
+
+# def display(x,y):
+#     return x+y
+# l1=[1,2,3,4,5,6,7,8,9,10]
+# l2=[1,2,3,4,5,6,7]
+# result=reduce(display,l1)
+# print(result)
+
+# l1=[1,2,3,4,5,6]
+# l2=[6,5,7,5,8,9]
+# x=map(lambda x,y : x**2+y**2,l1,l2)
+# print(list(x))
+
+# decorators
+
+# def greet(milf):
+#     def xc(): 
+#        print("good morning")
+#        milf()
+#        print("thanks for using")
+#     return xc
+
+    
+    
+# @greet    
+# def display():
+#     print("hello world")
+    
+# display()
+# @greet
+# def add(a,b):
+#     print(a+b)
+    
+# add(1,2)
+    
+
+    
+# class Book:
+#     def welcome(self):
+#         print("welcome to our library")
+     
+#     @staticmethod    
+#     def thanks():
+#         print("thanks for visit")
+    
+# obj=Book()
+# obj.welcome()
+# obj.thanks()
+
+
+# from abc import ABC,abstractmethod
+
+# class Computer(ABC):
+#     @abstractmethod
+#     def animal(self):
+#         pass
+# class cat_family(Computer):
+#     def cat(self):
+#         print("meow")
         
+# class dog_family(Computer):
+#     def cat(self):
+#         print("Bark")
+        
+# c1=cat_family()
+# c1.cat()
+
+# from abc import ABC, abstractmethod
+
+# class Animal(ABC):
+#     @abstractmethod
+#     def make_sound(self):
+#         pass
+
+# class Dog(Animal):
+#     def make_sound(self):
+#         print("Woof!")
+
+# class Cat(Animal):
+#     def make_sound(self):
+#         print("Meow!")
+
+
+# #dog = Dog()
+# cat=Cat()
+# cat.make_sound()     # Output: Woof!
+
+from abc import ABC,abstractmethod
+
+class BankApp(ABC):
+    def login(self):
+        print("login")
+    def logout(self):
+        print("logout")
+    def details(self):
+        print("user details")
+    @abstractmethod
+    def database(self):
+        pass
+    
+class WebPage(BankApp):
+    def database(self):
+        print("database connected")
+        
+w1=WebPage()
+w1.login()
+w1.logout() 
+w1.details()
+w1.database()   
+    
--- python/tempCodeRunnerFile.py
+++ python/tempCodeRunnerFile.py
@@ -1,9 +1,4 @@
-n=int(input("enter any number:"))
-fact=1
-i=n
-while(i>=1):
-    fact=i*fact
-    i=i-1
-print(fact)
     
-    
+@greet    
+def display():
+    print("hello world")
